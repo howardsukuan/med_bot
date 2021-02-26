53ff662058d69a999a672ffc7ca0a98556ca5c49
howardsukuan <56188677+howardsukuan@users.noreply.github.com>
2021-02-26 14:19:26 +0800

    Delete account_info.py

--- my_med_bot/account_info.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-accountInfoDICT = {
-"email":"jacksugood@gmail.com",
-"apikey":"gQgpMy3A$uxbRgPuIPL$Rf#^Q&W$oq4",
-"USERNAME":"jacksugood@gmail.com",
-"LOKI_KEY":"rsAG%59msucea$+=ikzrQ!x49u9vcE4",
-"DISCORD_TOKEN":"Nzk2NjY3ODg0NTE5Njg2MjA0.X_bQyQ.fEnRMS7EUwD0elGgmQT3fxKU2F0",
-"DISCORD_GUILD":"討論小圈圈"}
