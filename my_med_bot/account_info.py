1669908561fd1748366db1a14156795c4ae68c5f
RobinJLin <dreamhopeandfaith@gmail.com>
2021-03-12 16:21:54 +0800

    add LINE README

---
+++ my_med_bot/account_info.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+accountInfoDICT = {
+"email":"jacksugood@gmail.com",
+"apikey":"gQgpMy3A$uxbRgPuIPL$Rf#^Q&W$oq4",
+"USERNAME":"jacksugood@gmail.com",
+"LOKI_KEY":"uHYorxaYVhgeKv2PP*RiikNFFuU=_*v",
+"DISCORD_TOKEN":"Nzk2NjY3ODg0NTE5Njg2MjA0.X_bQyQ.8VZTnLHGCyCBHBd_7JitfXviQOI",
+"DISCORD_GUILD":"討論小圈圈",
+"LINE_ACCESS_TOKEN":"NoJcRJMfu32lvA47vukeVpDszUKT8AE7wa0bFYBgNPGLkHb2a/QXgYQ07fYjv8oGRRXTAgGa2F7ncjaxnpDfksGnKCigMvkw+EYtcz01xt9+MNszm+SSXb4wSwilkbCv0AmEwEckIWSqGno4l0OPqAdB04t89/1O/w1cDnyilFU=",
+"LINE_CHANNEL_SECRET":"3e841538e9b12fb36aa690788e5d1fdb"}
